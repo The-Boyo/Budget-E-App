e6073119e487b740d923858dbabcf314da94948e
Eliud Metto <eliudmetto93@gmail.com>
2022-11-25 11:02:19 +0300

    Updated Input.js and Input.css
    Added a different border style to the input elements to communicate to the user if they are inputing an income or expense

--- src/components/input/Input.js
+++ src/components/input/Input.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useCallback, useEffect, useRef, useState } from "react";
 import { connect } from 'react-redux';
 
 import { getInput } from "../../actions";
@@ -9,7 +9,10 @@ const Input = (props) => {
   const [description, setDescription] = useState('');
   const [value, setValue] = useState('');
   const [option, setOption] = useState('+');
+  const [borderDes, setBorderDes] = useState('1px solid black');
+  const [borderVal, setBorderVal] = useState('1px solid black');
 
+  const iconRef = useRef();
 
   const onFormSubmit = (e) => {
     e.preventDefault();
@@ -20,6 +23,11 @@ const Input = (props) => {
       props.getInput(description, value, option);
       setDescription('');
       setValue('');
+      iconRef.current.style.color = 'rgb(92, 230, 138)';
+
+      setInterval(()=>{
+        iconRef.current.style.color = 'black'
+      }, 1000)
     }
   }
   
@@ -34,24 +42,33 @@ const Input = (props) => {
   const onValueChange = (e) => {
     setValue(e.target.value)
   }  
-  
-  const inputRef = useRef();
-  const desRef = useRef();
-  const valRef = useRef();
-  const iconRef = useRef();
-  
+
+  const borderChange = (e) => {
+    if (e.target.className.includes('description')){
+      option === '+' ? setBorderDes('2px solid rgb(92, 230, 138)') : setBorderDes('2px solid rgb(233, 62, 77)')
+    } else if (e.target.className.includes('value')) {
+      option === '+' ? setBorderVal('2px solid rgb(92, 230, 138)') : setBorderVal('2px solid rgb(233, 62, 77)')
+    }
+  }
+
+  const removeBorder = () => {
+    setBorderDes('1px solid black');
+    setBorderVal('1px solid black')
+  }
+
 
   return (
     <form onSubmit={onFormSubmit} className="input-cont">
-      <select onChange={onOptionChange} className="select" ref = {inputRef}>
+      <select value={option} onChange={onOptionChange} className="select" >
         <option className="positive">+</option>
         <option className="negative">-</option>
       </select>
-      <input type="text" placeholder="description..." value={description} className={`description`} onChange={onDescriptionChange} ref={desRef} />
+      <input type="text" placeholder="description..." value={description} className={`description`} style={{border:borderDes}} onChange={onDescriptionChange} onFocusCapture={borderChange} onBlur={removeBorder} />
 
-      <input type="number" placeholder="value..." value={value} className={`value`} onChange={onValueChange} ref={valRef}  />
+      <input type="number" placeholder="value..." value={value} className={`value`} style={{border:borderVal}} onChange={onValueChange} onFocusCapture={borderChange} onBlur={removeBorder}  />
+      
       <button className="button">
-        <i className="fa-regular fa-circle-check" ref={iconRef}></i>
+        <i className="fa-regular fa-circle-check" ref={iconRef} ></i>
       </button>
     </form>
   )
